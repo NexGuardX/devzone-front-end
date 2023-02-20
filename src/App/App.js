51b0ffcd1d46ba6f951c8ebfcc6dce459e34499b
Flo <floriane.perucchini@gmail.com>
2023-02-20 15:46:12 +0100

    config : install react-router-dom

---
+++ src/App/App.js
@@ -0,0 +1,31 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>NAVBAR</div>
+      <Routes>
+        <Route path="/" element={<div>HOME</div>} />
+        <Route path="/login" element={<div>LOGIN</div>} />
+        <Route path="/signup" element={<div>SIGNUP</div>} />
+        <Route
+          path="/app"
+          element={
+            <div>
+              SIDEBAR
+              <Outlet />
+            </div>
+          }
+        >
+          <Route index element={<div>WELCOME APP(NEWS)</div>} />
+          <Route path="news" element={<div>NEWS</div>} />
+          <Route path="search" element={<div>SEARCH</div>} />
+          <Route path="playground" element={<div>PLAYGROUND</div>} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App/App';
 import './assets/styles/index.scss';
-import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
