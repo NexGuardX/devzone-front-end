245bd6f482694214d8f8b26660bc173d7188d54a
Abdelkarim <badelgeek@gmail.com>
2023-02-27 10:52:28 +0100

    feat(news): add sources, improve display, add loading

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,47 +1,74 @@
 import {
+  AspectRatio,
+  Avatar,
   Box,
   Button,
   Card,
   CardBody,
   CardFooter,
+  CardHeader,
   Divider,
+  Flex,
   Heading,
+  HStack,
+  IconButton,
   Image,
   SimpleGrid,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
+import { BsStar } from 'react-icons/bs';
 import { HiOutlineExternalLink } from 'react-icons/hi';
+import youtubeLogo from '../../assets/images/youtube.png';
 
 const { REACT_APP_APIEXT_URL } = process.env;
 
 const RSS_LIST = [
   'https://www.freecodecamp.org/news/rss',
+  'https://rss.app/feeds/rlIgUQu1yZMkJPSr.xml', // CSS tricks
+  'https://rss.app/feeds/ozZVa9oz323O11W4.xml', // dev.to
+  'https://rss.app/feeds/rBjanTnSHUwMc2vm.xml', // LogRocket
+  'https://rss.app/feeds/XXKIuLS9Zof1Hf1R.xml', // sitepoint
+  'https://rss.app/feeds/5O5NpvuF9es9Ne6W.xml', // smashingmagazine.com
+  'https://rss.app/feeds/XwYF0hX2DMrNhzog.xml', // web.dev
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCFbNIlppjAuEX4znoulh0Cw', // WebDevSimplified
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCsBjURrPoezykLs9EqgamOA', // Fireship
 ];
 // TODO : loading
-// TODO : Origin icon + name
 // TODO : Type of article
 // TODO : comment
 // TODO : jsdoc
-const parseAndSortFetchedData = (dataArray) =>
-  dataArray
-    .map((data) => ({
-      title: data.title,
-      date: data?.published || data?.pubDate,
-      image:
-        (data['media:content'] && data['media:content']['@_url']) ||
-        (data['media:group'] &&
-          data['media:group']['media:thumbnail'] &&
-          data['media:group']['media:thumbnail']['@_url']),
-      link: data.link,
+
+const getAuthorFromEntry = (entry) => entry['dc:creator'] || entry.author.name;
+const getImageFromEntry = (entry) =>
+  (entry['media:content'] && entry['media:content']['@_url']) ||
+  (entry['media:group'] &&
+    entry['media:group']['media:thumbnail'] &&
+    entry['media:group']['media:thumbnail']['@_url']) ||
+  entry?.enclosure['@_url'];
+
+const getDateFromEntry = (entry) =>
+  Date.parse(entry?.published || entry?.pubDate || entry?.lastBuildDate);
+
+const parseAndSortFetchedData = (entries) =>
+  entries
+    .map((entry) => ({
+      author: getAuthorFromEntry(entry),
+      title: entry.title,
+      date: getDateFromEntry(entry),
+      image: getImageFromEntry(entry),
+      link: entry.link,
+      logo: entry.logo,
     }))
-    .sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+    .sort((a, b) => b.date - a.date);
 
 export default function News() {
   const [fetchResult, setFetchResult] = useState([]);
-  console.log('⏩ ~ Content ~ fetchResult:', fetchResult);
+  const [isLoaded, setIsLoaded] = useState(0);
   const fetched = useRef([]);
 
   useEffect(() => {
@@ -57,38 +84,87 @@ export default function News() {
 
     Promise.all(promises).then((values) => {
       values.map((value) =>
-        value.json().then((json) =>
-          setFetchResult((r) => {
-            const items = json?.entry || json?.item;
-            return [...r, ...items];
+        value
+          .json()
+          .then((json) =>
+            setFetchResult((r) => {
+              // eslint-disable-next-line no-shadow
+              let items = json?.entry || json?.item;
+              items = items.map((item) => ({
+                ...item,
+                logo: json?.image?.url,
+              }));
+              return [...r, ...items];
+            })
+          )
+          .finally(() => {
+            console.log('loaded', isLoaded);
+            setIsLoaded(true);
           })
-        )
       );
     });
   }, []);
 
+  const entries = parseAndSortFetchedData(fetchResult);
+
   return (
-    <Box p="2rem">
+    <Box p="2rem" maxW="1200px" margin="0 auto">
       <Heading as="h1" pb="2rem">
         News
       </Heading>
+
       <SimpleGrid minChildWidth="320px" gap="2rem">
-        {parseAndSortFetchedData(fetchResult).map((item) => (
-          <Card key={item.date} border="1px solid" p="0.5rem" mb="3">
+        {Array(6)
+          .fill(0)
+          .map((skeleton, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <VStack display={isLoaded && 'none'} key={index}>
+              <SkeletonCircle isLoaded={isLoaded} size="10" fadeDuration={1} />
+              <Skeleton height="200px" isLoaded={isLoaded} fadeDuration={1} />
+              <SkeletonText
+                isLoaded={isLoaded}
+                mt="4"
+                noOfLines={4}
+                spacing="4"
+                skeletonHeight="3"
+                fadeDuration={1}
+              />
+            </VStack>
+          ))}
+        {entries.map((item) => (
+          <Card key={item.title} border="1px solid" p="0.5rem" mb="3">
+            <CardHeader>
+              <Flex justifyContent="space-between">
+                <Box>
+                  <HStack>
+                    <Avatar size="sm" name={item.author} src={item.logo || youtubeLogo} />
+                    <Text fontWeight="bold">{item.author}</Text>
+                  </HStack>
+                </Box>
+                <IconButton variant="ghost" icon={<BsStar />} />
+              </Flex>
+            </CardHeader>
             <CardBody>
-              <Image src={item.image} h="220px" w="100%" objectFit="cover" />
-              <Text>{item.date}</Text>
-              <Heading as="h2" fontSize="md">
-                {item.title}
-              </Heading>
+              <VStack align="stretch">
+                <AspectRatio maxW="400px" ratio={16 / 9}>
+                  <Image src={item.image} />
+                </AspectRatio>
+
+                <Heading as="h2" fontSize="md">
+                  {item.title}
+                </Heading>
+              </VStack>
+              <Text textAlign="left" fontSize="xs">
+                {new Date(item.date).toLocaleString().replace(',', '').split(' ').join(' - ')}
+              </Text>
             </CardBody>
             <Divider />
             <CardFooter>
-              <Button rightIcon={<HiOutlineExternalLink />}>
+              <Flex width="100%" justifyContent="flex-end">
                 <a href={item.link} target="_blank" rel="noreferrer">
-                  Link
+                  <Button rightIcon={<HiOutlineExternalLink />}>Show More</Button>
                 </a>
-              </Button>
+              </Flex>
             </CardFooter>
           </Card>
         ))}
