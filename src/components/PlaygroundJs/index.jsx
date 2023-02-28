6dcc21281fb3f8b760741a11ef3347fa738cc1f5
ZedToTech <azouaou.benadda@gmail.com>
2023-02-28 14:53:04 +0100

    fix: SplitePane for the playgroundJs

--- src/components/PlaygroundJs/index.jsx
+++ src/components/PlaygroundJs/index.jsx
@@ -37,12 +37,7 @@ export default function PlaygroundJs() {
   return (
     <Flex style={{ ...layoutCSS }}>
       <Hide above="md">
-        <SplitPane
-          split="horizontal"
-          sizes={sizes}
-          onChange={setSizes}
-          display={{ base: 'none', md: 'block' }}
-        >
+        <SplitPane split="horizontal" sizes={sizes} onChange={setSizes}>
           <Pane minSize="10%" maxSize="90%">
             <Box style={{ ...layoutCSS }}>
               <Editor language="javascript" code={code} setCode={setCode} />
