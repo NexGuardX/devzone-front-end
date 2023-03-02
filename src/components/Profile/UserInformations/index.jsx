bdb83f94703cffe22eb5fa8a1e56fc62c15fc921
floriane-perucchini <floriane.perucchini@gmail.com>
2023-03-02 16:46:31 +0100

    feat: edit profile - functionnal

--- src/components/Profile/UserInformations/index.jsx
+++ src/components/Profile/UserInformations/index.jsx
@@ -21,11 +21,12 @@ import { useRef, useState } from 'react';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { useDispatch } from 'react-redux';
 import {
-  setDescription,
   setEmail,
   setFirstname,
   setLastname,
+  setUsername,
   setWebsite,
+  thunkUpdateProfil,
 } from '../../../features/user/userSlice';
 import Description from './Description';
 
@@ -43,10 +44,13 @@ function UserInformations({ user }) {
   // TODO: Factoriser le handleSubmit et le userSlice
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(setFirstname(event.target[0].value));
-    dispatch(setLastname(event.target[1].value));
-    dispatch(setWebsite(event.target[2].value));
-    dispatch(setEmail(event.target[3].value));
+    const id = event.target[0].value;
+    dispatch(thunkUpdateProfil(form, id));
+    dispatch(setUsername(event.target[1].value));
+    dispatch(setFirstname(event.target[2].value));
+    dispatch(setLastname(event.target[3].value));
+    dispatch(setWebsite(event.target[4].value));
+    dispatch(setEmail(event.target[5].value));
     onClose();
   };
 
@@ -90,6 +94,25 @@ function UserInformations({ user }) {
           <ModalCloseButton />
           <ModalBody pb={6}>
             <form onSubmit={handleSubmit} className="modal-form">
+              <FormControl>
+                <Input type="hidden" name="id" value={user.id} />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel className="modal-form-label" htmlFor="username">
+                  Username
+                  <Input
+                    id="username"
+                    onChange={handleChange}
+                    ref={initialRef}
+                    type="text"
+                    name="username"
+                    defaultValue={user.username}
+                    placeholder="Username"
+                  />
+                </FormLabel>
+              </FormControl>
+
               <FormControl>
                 <FormLabel className="modal-form-label" htmlFor="firstname">
                   First name
@@ -147,6 +170,19 @@ function UserInformations({ user }) {
                 </FormLabel>
               </FormControl>
 
+              <FormControl>
+                <FormLabel className="modal-form-label" htmlFor="website">
+                  Password
+                  <Input
+                    id="password"
+                    onChange={handleChange}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </FormLabel>
+              </FormControl>
+
               <Button type="submit">Save</Button>
               <Button type="button" onClick={onClose}>
                 Cancel
@@ -160,6 +196,7 @@ function UserInformations({ user }) {
 }
 UserInformations.propTypes = {
   user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     avatar: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     firstname: PropTypes.string,
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,6 +2,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const { REACT_APP_API_URL } = process.env;
+
 const initialState = {
   username: '',
 };
@@ -28,8 +30,14 @@ export const userSlice = createSlice({
     setDescription: (state, action) => {
       state.description = action.payload;
     },
+    setId: (state, action) => {
+      state.id = action.payload;
+    },
     setToolsUser: (state, action) => {
       state.tools = action.payload;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
     },
     removeToolsUser: (state, action) => {
       state.tools = action.payload;
@@ -52,6 +60,8 @@ export const {
   setFirstname,
   setWebsite,
   setEmail,
+  setId,
+  setToken,
   setDescription,
   logout,
 } = userSlice.actions;
@@ -60,15 +70,17 @@ const thunkLogin =
   ({ email, password }) =>
   async (dispatch) => {
     try {
-      const response = await axios.post('http://localhost:5050/login', {
+      const response = await axios.post(`${REACT_APP_API_URL}/login`, {
         email,
         password,
       });
+      dispatch(setId(response.data.user.id));
       dispatch(setFirstname(response.data.user.firstname));
       dispatch(setLastname(response.data.user.lastname));
       dispatch(setEmail(response.data.user.email));
       dispatch(setUsername(response.data.user.username));
-      console.log(response.data.user);
+      dispatch(setToken(response.data.token.accessToken));
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -81,7 +93,7 @@ export const thunkSignup =
     const SignupForm = { username, email, password, confirmedPassword };
     dispatch(setSignupForm(SignupForm));
     try {
-      const response = await axios.post('http://localhost:5050/signup', {
+      const response = await axios.post(`${REACT_APP_API_URL}/signup`, {
         username,
         email,
         password,
@@ -92,3 +104,25 @@ export const thunkSignup =
       console.log(error);
     }
   };
+
+export const thunkUpdateProfil = (form, id) => async (dispatch) => {
+  const { username, email, firstname, lastname, website, password } = form;
+  try {
+    const response = await axios.patch(`${REACT_APP_API_URL}/user/${id}`, {
+      username,
+      email,
+      firstname,
+      lastname,
+      website,
+      password,
+    });
+    dispatch(setFirstname(response.data.user.firstname));
+    dispatch(setLastname(response.data.user.lastname));
+    dispatch(setEmail(response.data.user.email));
+    dispatch(setUsername(response.data.user.username));
+    dispatch(setWebsite(response.data.website));
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
