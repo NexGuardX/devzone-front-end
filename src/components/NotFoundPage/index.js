9156e42ffc71ecdf2f64bd010fb70f1025c0d118
ZedToTech <azouaou.benadda@gmail.com>
2023-03-03 12:06:38 +0100

    fix: close #36

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,4 +1,4 @@
-import { Button, Center, Img, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Center, Img, Text, VStack } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 import Delorean from '../../assets/images/DeLorean4.png';
 
@@ -6,19 +6,23 @@ function NotFoundPage() {
   return (
     <Center>
       <VStack>
-        <Text color="gray" fontSize="9xl">
+        <Text color="gray" fontSize="200px">
           404
         </Text>
-        <Img src={Delorean} alt="Dolorean" />
-        <Text color="gray" fontSize="2xl" textAlign="center">
-          I GUESS YOU AREN&apos;T READY FOR THAT PAGE YET...{' '}
-        </Text>
-        <Text color="gray" fontSize="2xl" textAlign="center">
-          BUT YOUR KIDS ARE GONNA LOVE IT.{' '}
-        </Text>
-        <NavLink to="/">
-          <Button colorScheme="teal">GO BACK</Button>
-        </NavLink>
+        <Box top="190" position="absolute">
+          <Img src={Delorean} alt="Dolorean" />
+          <Text color="gray" fontSize="1xl" textAlign="center">
+            I GUESS YOU AREN&apos;T READY FOR THAT PAGE YET...{' '}
+          </Text>
+          <Text color="gray" fontSize="1xl" textAlign="center">
+            BUT YOUR KIDS ARE GONNA LOVE IT.{' '}
+          </Text>
+          <Center>
+            <NavLink to="/">
+              <Button colorScheme="teal">GO BACK</Button>
+            </NavLink>
+          </Center>
+        </Box>
       </VStack>
     </Center>
   );
